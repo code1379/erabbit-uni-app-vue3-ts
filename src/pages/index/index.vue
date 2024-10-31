fe796ba8bd0a66b770fcd5e44dae199324d3099a
code1379 <coderxialuo@gmail.com>
2024-10-31 21:16:33 +0800

    feat: 在项目中如何使用 uni-ui

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -3,7 +3,19 @@
 </script>
 
 <template>
-  <view class="index">index</view>
+  <!-- 解决这个报错需要安装 Vue - Official v2.0.12 版本即可 -->
+  <view class="index">
+    <uni-section title="基础卡片" type="line">
+      <uni-card
+        title="基础卡片"
+        sub-title="副标题"
+        extra="额外信息"
+        thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
+      >
+        <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
+      </uni-card>
+    </uni-section>
+  </view>
 </template>
 
 <style lang="scss">
