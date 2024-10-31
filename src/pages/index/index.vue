65d760a486f710b125da3e063924579976992176
code1379 <coderxialuo@gmail.com>
2024-11-01 01:00:17 +0800

    feat: 轮播图

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,11 +1,27 @@
 <script setup lang="ts">
+import { getHomeBannerAPI } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+
+const bannerList = ref<BannerItem[]>([])
+const getHomeBannerData = async () => {
+  const res = await getHomeBannerAPI()
+  console.log('res', res)
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <!-- 解决这个报错需要安装 Vue - Official v2.0.12 版本即可 -->
   <view class="index">
     <CustomNavbar />
+    <XtxSwiper :list="bannerList" />
   </view>
 </template>
 
