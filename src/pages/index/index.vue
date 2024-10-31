b711d643b0df3d87502274af83836f8ef365b1ca
code1379 <coderxialuo@gmail.com>
2024-10-31 22:31:06 +0800

    feat: 自定义导航栏 + 安全区域

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,20 +1,11 @@
 <script setup lang="ts">
-//
+import CustomNavbar from './components/CustomNavbar.vue'
 </script>
 
 <template>
   <!-- 解决这个报错需要安装 Vue - Official v2.0.12 版本即可 -->
   <view class="index">
-    <uni-section title="基础卡片" type="line">
-      <uni-card
-        title="基础卡片"
-        sub-title="副标题"
-        extra="额外信息"
-        thumbnail="https://qiniu-web-assets.dcloud.net.cn/unidoc/zh/unicloudlogo.png"
-      >
-        <text>这是一个带头像和双标题的基础卡片，此示例展示了一个完整的卡片。</text>
-      </uni-card>
-    </uni-section>
+    <CustomNavbar />
   </view>
 </template>
 
